56a29093d0ed752a54f7b73c70042f4c3cc61871
Atif Galamov <kalemli@gmail.com>
2020-07-24 19:58:17 +0400

    Updates

---
+++ precache-manifest.f660aa21bb05ff1ec4b7c0f862875dbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35ffa54f78fdf46311f81b9fde235f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aef2deb3fbd1452c2b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "575d3495337b26d1dbb1",
+    "url": "/static/js/2.4864f733.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4864f733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aef2deb3fbd1452c2b8",
+    "url": "/static/js/main.59f493ed.chunk.js"
+  },
+  {
+    "revision": "0de845ed36c41eb5f90f",
+    "url": "/static/js/runtime-main.9a5e2046.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
